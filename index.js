7e87f8390439030f62055c650704e06b295b517c
Steven Radig <sjradig@gmail.com>
2019-10-13 16:05:18 -0500

    commiting all project files

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log('Name: Steven Radig');
+console.log('Career: Frontend Developer');
+console.log('Description: Frontend developer with three years experience looking to go fulltack.');
+
+console.group('My Interests:');
+console.log('* Web Development');
+console.log('* Scrabble');
+console.log('* Fortnite');
+console.log('* Basketball');
+console.log('* Football');
+console.log('* Sports in general I guess');
+console.log('* Music !(Jazz || Mumble Rap || Screamo)');
+console.groupEnd();
+
+console.group('My Previous Experience:');
+console.log('* Metro Studios | Frontend Developer - Created websites for a broad base of clients in a variety of industry based on mocks given by a designer');
+console.log('* Porter Moving Company | Mover - Picked up heavy things, put them down in a truck. Then picked them up again and put them down in a different location. Operated long vehicles.');
+console.log('* Viridian Lawn Care | Owner/Operator - Provided lawn care and property maintenance services for several dozen residential and commercial accounts.');
+console.log('* Pearson | Web Intern - Modified XML documents representing standardized test items to specification as part of a large national contract involving dozens of States transitioning to computer based standardized testing. One of three interns to stay on beyond original term.');
+console.log('* Southwestern Advantage | Sales Professional - Approached and prospected over three thousand families selling printed and digital educational materials. Executed all ordering and deliveries in a thirteen week relocation to another state.');
+console.log('* Family Heritage | Sales Professional - Approached and prospected families of various socioeconomic backgrounds building rapport and protecting them from the financial effects of cancer and critical illness by enrolling them in supplemental health insurance policies.');
+console.groupEnd();
+
+console.group('My Skills:');
+console.log('HTML');
+console.log('CSS');
+console.log('CSS Preprocessors');
+console.log('Javascript');
+console.log('Gulp');
+console.log('DOM Manipulation/Traversal');
+console.log('Bootstrap');
+console.log('Responsive Design');
+console.log('Crossbrowser Compatibility');
+console.groupEnd();
